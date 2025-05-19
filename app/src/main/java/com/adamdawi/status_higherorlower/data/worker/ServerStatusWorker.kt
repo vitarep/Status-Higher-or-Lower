be08cc7a87d06ebc9baca45353a71388861ae464
frugoz05 <adamdawidziuk@onet.pl>
2025-05-19 14:57:51 +0200

    NEW:PendingIntent to server status notification to open app on notification click

--- app/src/main/java/com/adamdawi/status_higherorlower/data/worker/ServerStatusWorker.kt
+++ app/src/main/java/com/adamdawi/status_higherorlower/data/worker/ServerStatusWorker.kt
@@ -3,12 +3,15 @@ package com.adamdawi.status_higherorlower.data.worker
 import android.R.drawable.ic_dialog_alert
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.adamdawi.status_higherorlower.MainActivity
 import com.adamdawi.status_higherorlower.domain.ServerStatusRepository
 import com.adamdawi.status_higherorlower.domain.model.ServerStatus
 
@@ -45,6 +48,14 @@ class ServerStatusWorker(
         val notificationManager =
             applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
         // Android 8+ requires notification channel
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
@@ -60,6 +71,8 @@ class ServerStatusWorker(
             .setContentText(message)
             .setSmallIcon(ic_dialog_alert)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
             .build()
 
         notificationManager.notify(1, notification)
